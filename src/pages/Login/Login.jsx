79a6ff77aa93616f3de44facd98ba7b496fbd483
CarlosCesarO <carlosc.ogoncalves@gmail.com>
2024-01-02 22:53:59 -0300

    login e signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,42 +1,57 @@
 import React from "react";
 import { Button } from "@/shadcn/components/ui/button";
 import { Input } from "@/shadcn/components/ui/input";
+import Logo from "../../assets/logo.svg";
 
 export default function Login() {
   return (
-    <div className="flex gap-20 h-screen w-full p-5">
-      <div className="w-1/2 bg-muted rounded-lg p-5">
-        <h2 className="text-3xl font-semibold">
+    <div className="flex gap-20 h-screen w-full py-20 px-40">
+      <div className="w-1/2 bg-muted rounded-lg p-12">
+        <div className="flex items-center gap-3">
+          <img className="h-8" src={Logo} alt="logo" />
+          <h2 className="text-2xl tracking-widest font-medium mb-0.5">
+            get<span className="text-primary">it</span>done.
+          </h2>
+        </div>
+        <h2 className="mt-24 text-3xl leading-[50px] font-semibold">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit.
         </h2>
-        <p>
+        <p className="mt-10 text-muted-foreground">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quia
           tempore mollitia labore rem necessitatibus ullam enim ea deserunt!
           Dolores labore nam voluptas alias repellendus nihil maxime distinctio
           veritatis sed?
         </p>
-        <div>
+        <div className="bg-foreground text-background p-8 rounded-xl mt-16 leading-8">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat,
           illo. At aperiam earum sed optio aspernatur. Commodi natus, nemo
           veritatis voluptatum quos enim deleniti temporibus nam. Aliquam, iure.
           Ratione, fugiat!
         </div>
       </div>
-      <div className="w-1/2">
-        <h1 className="text-2xl font-semibold ">Cadastre-se agora</h1>
-        <p className="mt-4 text-muted-foreground font-normal text-lg">
-          Crie sua conta agora mesmo
-        </p>
-        <form className="mt-10">
-          <p className="text-muted-foreground mb-2.5">Nome Completo</p>
-          <Input />
-          <p className="mt-5 text-muted-foreground mb-2.5">E-mail</p>
-          <Input />
-          <p className="mt-5 text-muted-foreground mb-2.5">Senha</p>
-          <Input />
-        </form>
+      <div className="flex flex-col justify-center w-1/2 px-20">
+        <div>
+          <h1 className="text-3xl font-semibold ">Entre na sua conta</h1>
+          <p className="mt-4 text-muted-foreground font-normal text-lg">
+            Informe seus dados de acesso
+          </p>
+          <form className="mt-10">
+            <p className="mt-5 text-muted-foreground mb-2.5">E-mail</p>
+            <Input type="email" />
+            <p className="mt-5 text-muted-foreground mb-2.5">Senha</p>
+            <Input type="password" />
+            <Button size="xl" className="mt-10 text-lg w-full">
+              Entrar na minha conta
+            </Button>
+          </form>
+          <div className="flex justify-center gap-2 text-lg mt-12">
+            <p>Não tem uma conta?</p>
+            {/* <Link to="/signup" className="text-primary">
+              Cadastre-se agora.
+            </Link> */}
+          </div>
+        </div>
       </div>
-      <Button className="h-10 p-5">Entrar na minha conta</Button>
     </div>
   );
 }
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,5 +1,60 @@
 import React from "react";
+import { Button } from "@/shadcn/components/ui/button";
+import { Input } from "@/shadcn/components/ui/input";
+import Logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="flex gap-20 h-screen w-full py-20 px-40">
+      <div className="w-1/2 bg-muted rounded-lg p-12">
+        <div className="flex items-center gap-3">
+          <img className="h-8" src={Logo} alt="logo" />
+          <h2 className="text-2xl tracking-widest font-medium mb-0.5">
+            get<span className="text-primary">it</span>done.
+          </h2>
+        </div>
+        <h2 className="mt-24 text-3xl leading-[50px] font-semibold">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        </h2>
+        <p className="mt-10 text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quia
+          tempore mollitia labore rem necessitatibus ullam enim ea deserunt!
+          Dolores labore nam voluptas alias repellendus nihil maxime distinctio
+          veritatis sed?
+        </p>
+        <div className="bg-foreground text-background p-8 rounded-xl mt-16 leading-8">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat,
+          illo. At aperiam earum sed optio aspernatur. Commodi natus, nemo
+          veritatis voluptatum quos enim deleniti temporibus nam. Aliquam, iure.
+          Ratione, fugiat!
+        </div>
+      </div>
+      <div className="flex flex-col justify-center w-1/2 px-20">
+        <div>
+          <h1 className="text-3xl font-medium">Cadastre-se agora</h1>
+          <p className="mt-4 text-muted-foreground font-normal text-lg">
+            Crie sua conta agora mesmo
+          </p>
+          <form className="mt-10">
+            <p className="text-muted-foreground mb-2.5">Nome Completo</p>
+            <Input type="text" />
+            <p className="mt-5 text-muted-foreground mb-2.5">E-mail</p>
+            <Input type="email" />
+            <p className="mt-5 text-muted-foreground mb-2.5">Senha</p>
+            <Input type="password" />
+            <Button size="xl" className="mt-10 text-lg w-full">
+              Criar minha conta
+            </Button>
+          </form>
+          <div className="flex justify-center gap-2 text-lg mt-12">
+            <p>Já tem uma conta?</p>
+            <Link to="/login" className="text-primary">
+              Entre agora.
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
