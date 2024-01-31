c9849447a6f6fbdb5c142f45539042e9939dd20c
CarlosCesarO <carlosc.ogoncalves@gmail.com>
2024-01-31 11:47:41 -0300

    trocando a imagem do avatar

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -12,7 +12,10 @@ export default function Profile() {
   const { user } = useAuthContext();
 
   const inputRef = useRef();
-  const openFileSelector = () => input.current.click();
+  const openFileSelector = () => inputRef.current.click();
+  const openFileSelected = (e) => {
+    const selecteFile = e.target.files[0];
+  };
 
   return (
     <div className="p-5">
@@ -20,11 +23,16 @@ export default function Profile() {
       <div className="mb-10">
         <Avatar className="h-16 w-16" role="button" onClick={openFileSelector}>
           <AvatarImage src="" />
-          <AvatarFallback className="bg-primary/50 text-xl">
+          <AvatarFallback className="bg-primary/50 text-2xl">
             {getInitials(user.displayName)}
           </AvatarFallback>
         </Avatar>
-        <input ref={inputRef} type="file" className="hidden" />
+        <input
+          onChange={openFileSelected}
+          ref={inputRef}
+          type="file"
+          className="hidden"
+        />
       </div>
       <ModeToggle />
     </div>
