be8607ac532158dec6bb10d3228fe7b0e2947d9a
CarlosCesarO <carlosc.ogoncalves@gmail.com>
2024-03-19 13:31:06 -0300

    tentando resolver problema na criacao de novo usuario

--- functions/onUserCreate.js
+++ functions/onUserCreate.js
@@ -1,5 +1,11 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+mailchimp.setConfig({
+  apiKey: functions.config().mailchimp.apiKey,
+  server: "us12",
+});
 
 if (admin.apps.length === 0) {
   admin.initializeApp();
@@ -8,10 +14,21 @@ if (admin.apps.length === 0) {
 exports.onUserCreate = functions.firestore
   .document("users/{userId}")
   .onCreate(async (snap, context) => {
-    const userId = context.params.userId;
-    const data = snap.data();
+    const userData = snap.data();
+    const email = userData.email;
 
-    console.log(userId);
-
-    return null;
+    const listId = "14ac8d6a41";
+    const subscribingUser = {
+      firstName: userData.name.split(" ")[0],
+      lastName: userData.name.split(" ")[1],
+      email,
+    };
   });
+
+async function updateTags(subscriberHash, tags) {
+  const response = await mailchimp.lists.updateMemberTags(
+    listId,
+    subscriberHash,
+    { tags: [...tags] }
+  );
+}
